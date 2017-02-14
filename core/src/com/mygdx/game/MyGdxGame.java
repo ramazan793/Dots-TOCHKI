6baf71b27258dfbbaa7ab166aba3cc656b14bce8
ramazan793 <demonmail.ru00@gmail.com>
2017-02-14 15:39:00 +0300

    DED

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,15 +2,24 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import static com.badlogic.gdx.scenes.scene2d.InputEvent.Type.touchUp;
 
 
 public class MyGdxGame extends ApplicationAdapter {
@@ -20,29 +29,142 @@ public class MyGdxGame extends ApplicationAdapter {
 	Actor dot;
 	Vector2 touchPos;
 	BitmapFont out;
+	Stage stage;
+	Viewport viewport;
+	Texture red;
+	Actor reddot;
+
+	/*class MyInputListener implements InputProcessor {
+
+		@Override
+		public boolean keyDown(int keycode) {
+			return false;
+		}
+
+		@Override
+		public boolean keyUp(int keycode) {
+			return false;
+		}
+
+		@Override
+		public boolean keyTyped(char character) {
+			return false;
+		}
+
+		@Override
+		public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+			return false;
+		}
+
+		@Override
+		public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+			if (screenX == (int)dot.getX() && screenY == 1080-(int)dot.getY() ) {
+				dot = new Dot();
+				dot.setPosition(0, 0);
+				dot.setSize(40, 40);
+				stage.addActor(dot);
+			}
+			return true;
+
+		}
+
+		@Override
+		public boolean touchDragged(int screenX, int screenY, int pointer) {
+			return false;
+		}
+
+		@Override
+		public boolean mouseMoved(int screenX, int screenY) {
+			return false;
+		}
+
+		@Override
+		public boolean scrolled(int amount) {
+			return false;
+		}
+	}
+	*/
+	class ActorListener extends InputListener {
+		@Override
+		public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+			//event.getListenerActor().setSize(80, 80);
+			event.getListenerActor().setColor(Color.BLUE);
+			return true;
+
+		}
+		/*@Override
+		public void touchUp(InputEvent event, float x, float y, int pointer, int button){
+			event.getListenerActor().setSize(120,120);
+		}*/
+
+	}
+
 
 	class Dot extends Actor {
 
-		public void draw(SpriteBatch batch, float parentAlpha){
+		@Override
+		public void draw(Batch batch, float parentAlpha) {
+			Color batchColor = batch.getColor();
+
+			final float r = batchColor.r;
+			final float g = batchColor.g;
+			final float b = batchColor.b;
+			final float a = batchColor.a;
+
+			Color dotColor = getColor();
+
+			batch.setColor(dotColor.r, dotColor.g, dotColor.b, dotColor.a * parentAlpha);
 			batch.draw(dotimg, getX(), getY(), getWidth(), getHeight());
+			batch.setColor(r, g, b, a);
 		}
 	}
+	class RedDot extends Actor {
+		@Override
+		public  void draw(Batch batch, float parentAlpha){
+			batch.draw(red, getX(), getY(), getWidth(), getHeight());
+		}
+	}
+	public void initDots(){
+		for (int n1=0; n1 < 8; n1++) {
+			for (int n = 0; n < 14; n++) {
+
+				dot = new Dot();
+				dot.addListener(new ActorListener());
+				dot.setSize(40, 40);
+				dot.setPosition(120-20 + n * (120), 120-20 + n1 * (120));
+				stage.addActor(dot);
+			}
+		}
+	}
+
+
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		dotimg = new Texture("dotimg.png");
+		dotimg = new Texture("circle.png");
+		red = new Texture("white.png");
 		camera = new OrthographicCamera();
 		camera.setToOrtho(true, 1920, 1080);
-		dot = new Actor();
+		viewport = new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		stage = new Stage(viewport, batch);
+		dot = new Dot();
+		Gdx.input.setInputProcessor(stage);
+		initDots();
+		reddot = new RedDot();
+		reddot.setPosition(0,0);
+		reddot.setSize(120,120);
+		reddot.setColor(247,2,2,1);
 
-		dot.setX(0);
-		dot.setY(0);
+
+		/*(dot.setSize(40,40);
+		dot.setPosition(120,120);
+		stage.addActor(dot); */
 
 
-		touchPos = new Vector2();
 
-		out = new BitmapFont();
+
+
 
 
 	}
@@ -52,11 +174,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render() {
 		Gdx.gl.glClearColor(255, 255, 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		if (Gdx.input.isTouched()) {
-			batch.draw(dotimg, dot.getX(), dot.getY());
-		}
-		batch.end();
+		stage.draw();
+		stage.act(Gdx.graphics.getDeltaTime());
 	}
 
 	@Override
