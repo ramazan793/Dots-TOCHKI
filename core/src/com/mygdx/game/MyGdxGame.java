d43562cc99f362aa57fbcbe9f8c4fe95dc35a734
ramazan793 <demonmail.ru00@gmail.com>
2017-02-18 17:55:55 +0300

    DED

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,7 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -10,31 +10,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-
-import static com.badlogic.gdx.scenes.scene2d.InputEvent.Type.touchUp;
-import static com.badlogic.gdx.scenes.scene2d.ui.Table.Debug.actor;
-
+import java.util.ArrayList;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture dotimg;
 	OrthographicCamera camera;
 	Actor dot;
-	Vector2 touchPos;
 	BitmapFont out;
 	Stage stage;
 	Viewport viewport;
-	Texture red;
 	public int count=0;
-
+	Texture backg;
+	ArrayList<Red> reddots;
+	int ind;
+	BitmapFont font;
 
 
 
@@ -44,17 +40,19 @@ public class MyGdxGame extends ApplicationAdapter {
 			event.getListenerActor().remove();
 			float xx = event.getListenerActor().getX();
 			float yy = event.getListenerActor().getY();
+			ind = ((int)xx-46)/69;
 			if (count%2==0) {
 				Red r = new Red();
-				r.setSize(40,40);
+				r.setSize(30,30);
 				r.setPosition(xx,yy);
 				r.setColor(Color.RED);
+				//reddots.add(ind,r);
 				stage.addActor(r);
 
 
 			} else {
 				Blue b = new Blue();
-				b.setSize(40,40);
+				b.setSize(30,30);
 				b.setPosition(xx,yy);
 				b.setColor(Color.BLUE);
 				stage.addActor(b);
@@ -90,14 +88,14 @@ public class MyGdxGame extends ApplicationAdapter {
 	class Red extends Dot{} class Blue extends Dot{} //material
 
 	public void initDots(){
-		for (int n1=0; n1 < 8; n1++) {
-			for (int n = 0; n < 14; n++) {
+		for (int n1=0; n1 < 18; n1++) {
+			for (int n = 0; n < 25; n++) {
 
 				dot = new Dot();
 				dot.addListener(new DotListener());
-				dot.setSize(40, 40);
-				dot.setColor(255,255,255,1);
-				dot.setPosition(120-20 + n * (120), 120-20 + n1 * (120));
+				dot.setSize(35, 35);
+				dot.setPosition(61-15 + n * (69), 53-15 + n1 * ((float)57.5));
+				dot.setColor(0,0,0,0);
 				stage.addActor(dot);
 
 			}
@@ -110,18 +108,15 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void create() {
 		batch = new SpriteBatch();
 		dotimg = new Texture("circle.png");
+		backg = new Texture("backg.jpg");
 		camera = new OrthographicCamera();
 		camera.setToOrtho(true, 1920, 1080);
 		viewport = new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		stage = new Stage(viewport, batch);
 		Gdx.input.setInputProcessor(stage);
 		initDots();
-
-
-
-		/*(dot.setSize(40,40);
-		dot.setPosition(120,120);
-		stage.addActor(dot); */
+		font = new BitmapFont();
+		reddots =  new ArrayList();
 
 
 
@@ -136,6 +131,10 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render() {
 		Gdx.gl.glClearColor(255, 255, 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(backg,0,0,1920,1080);
+		font.draw(batch, "ind", 100, 100);
+		batch.end();
 		stage.draw();
 		stage.act(Gdx.graphics.getDeltaTime());
 	}
